3b64e4c75a94eea3fcb4345e4bf695745f7084a5
santii810 <santii810@hotmail.com>
2017-03-03 20:25:37 +0100

    Actualizado usermapper

--- app/src/main/java/Model/UserMapper.java
+++ app/src/main/java/Model/UserMapper.java
@@ -1,7 +1,5 @@
 package model;
 
-import android.util.Log;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,28 +18,30 @@ public class UserMapper {
     private static int countUsers;
     private final static String TAG = "UserMapper";
 
-    public UserMapper() {
+    public UserMapper(final User currentUser) {
         mDatabase = FirebaseDatabase.getInstance().getReference("users");
-        DatabaseReference countUsersReference = mDatabase.child("count_users");
-        countUsersReference.addValueEventListener(new ValueEventListener() {
+
+
+        // busca en bbdd el currentUSer
+        mDatabase.child(currentUser.getId()).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                countUsers = dataSnapshot.getValue(Integer.class);
-                Log.e(TAG, "onDataChange: ");
+            public void onDataChange(DataSnapshot snapshot) {
+                if (snapshot.getValue() == null) {
+                    UserMapper.add(currentUser);
+                } else {
+                    //TODO añadir puntos por logueo si la fecha actual es distinta a la ultima
+                }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-//TODO mostrar mensaje de que no hay conexion
- //               Snackbar.make(this, "Conexion perdida",Snackbar.LENGTH_LONG).show();
+                // TODO error de que no hay conexion
             }
         });
     }
 
 
     public static void add(User usuario) {
-Log.e(TAG,usuario.getId());
-
         Map<String, Object> postValues = usuario.toMap();
         Map<String, Object> childUpdates = new HashMap<>();
         //put(key,object)
@@ -51,5 +51,4 @@ Log.e(TAG,usuario.getId());
         mDatabase.updateChildren(childUpdates);
 
     }
-
 }
